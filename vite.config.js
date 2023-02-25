0264f72be35802d94f3c2b92109a07c206193118
Dan <dan.phillips100@gmail.com>
2023-02-25 16:15:52 +0000

    Updated vite.config to solve refresh redirect

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: "rewrite-slash-to-index-html",
+    apply: "serve",
+    enforce: "post",
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use("/", (req, _, next) => {
+        if (req.url === "/") {
+          req.url = "/index.html";
+        }
+        next();
+      });
+    },
+  };
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: "http://localhost:3001",
+  appType: "mpa", // disable history fallback
+  plugins: [rewriteSlashToIndexHtml()],
 });
