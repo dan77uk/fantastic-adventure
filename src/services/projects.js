7c84d5eece286af089e0d7ad714978980af711e5
Dan <dan.phillips100@gmail.com>
2023-02-25 16:34:10 +0000

    feat: updated baseURL in services

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const baseUrl = "/api/projects";
-const baseUrl = "http://localhost:3001/api/projects";
-// const baseUrl = "https://whatknot-api.onrender.com/api/projects";
+// const baseUrl = "http://localhost:3001/api/projects";
+const baseUrl = "https://whatknot-api.onrender.com/api/projects";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
